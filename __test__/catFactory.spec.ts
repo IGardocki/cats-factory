4d106427a9fa7e5fd21088ea5ab045b10880868a
Ian Gardocki <ian.d.gardocki@gmail.com>
2023-09-06 21:49:13 -0500

    wip

--- __test__/catFactory.spec.ts
+++ __test__/catFactory.spec.ts
@@ -2,22 +2,27 @@ import {
     Cat,
     Romad,
     Mimi,
-    CatFactory,
-    RomadFactory,
-    MimiFactory,
+    catFactory,
+    // RomadFactory,
+    // MimiFactory,
   } from '../catFactory'; // Import your classes and interfaces here
   
   describe('catFactory', () => {
     it('should create Romad who screams for food', () => {
-      const romadFactory: CatFactory = new RomadFactory();
-      const romad: Cat = romadFactory.createCat();
+      // const romadFactory: CatFactory = new RomadFactory();
+      const romad = catFactory('Romad');
       expect(romad.action()).toBe("scream for food");
     });
   
     it('should create Mimi who bites my toes at 3 am', () => {
-      const mimiFactory: CatFactory = new MimiFactory();
-      const mimi: Cat = mimiFactory.createCat();
+      // const mimiFactory: catFactory = new MimiFactory();
+      const mimi = catFactory('Mimi');
       expect(mimi.action()).toBe("bite my toes at 3 am");
     });
+
+    it('should throw an error with a cat that is not Romad or Mimi', () => {
+      // @ts-ignore
+      expect(catFactory('notACat')).toThrowError('Invalid cat!!!');
+    })
   });
   
--- catFactory.ts
+++ catFactory.ts
@@ -16,31 +16,70 @@ export class Mimi implements Cat {
   }
 }
 
-// Cat Factory interface
-export interface CatFactory {
-  createCat(): Cat;
-}
+// type CatName = 'Mimi' | 'Romad'
 
-// Concrete Cat Factories
-export class RomadFactory implements CatFactory {
-  createCat(): Cat {
-    return new Romad();
+// export interface CatFactory {
+//   createCat(catname: CatName ): Cat {
+//     if(catname === 'Romad'){
+//       return new Romad()
+//     }
+//     else {
+//       return new Mimi()
+//     }
+//   }
+// };
+
+// // Cat Factory interface
+// export interface ICatFactory implements CatFactory{
+//   createCat(name: CatName): Cat;
+// }
+
+// // // Concrete Cat Factories
+// // export class RomadFactory implements CatFactory {
+// //   createCat(): Cat {
+// //     return new Romad();
+// //   }
+// // }
+
+// // export class MimiFactory implements CatFactory {
+// //   createCat(): Cat {
+// //     return new Mimi();
+// //   }
+// // }
+
+// // Client code
+// ICatFactory.createCat('Roamd')
+// const catFactory = new ICatFactory();
+// const romad = catFactory.createCat('Romad');
+// // const romadFactory: CatFactory = new RomadFactory();
+// // const romad: Cat = romadFactory.createCat();
+// console.log('Romad likes to', romad.action()); 
+// // Romad screams for food
+
+// // const mimiFactory: CatFactory = new MimiFactory();
+// const mimi = catFactory.createCat('Mimi');
+// console.log('Mimi likes to', mimi.action()); 
+// // Mimi bites my toes at 3 am
+
+// const notACat = catFactory.createCat('notACat');
+// // console.log('Mimi likes to', mimi.action()); 
+// // Mimi bites my toes at 3 am
+
+type CatName = 'Mimi' | 'Romad'
+
+export function catFactory(type: CatName): Cat {
+  switch (type) {
+    case 'Romad':
+      return new Romad()
+    case 'Mimi':
+      return new Mimi();
+    default:
+      throw new Error('Invalid cat!!!');
   }
 }
 
-export class MimiFactory implements CatFactory {
-  createCat(): Cat {
-    return new Mimi();
-  }
-}
-
-// Client code
-const romadFactory: CatFactory = new RomadFactory();
-const romad: Cat = romadFactory.createCat();
-console.log('Romad likes to', romad.action()); 
-// Romad screams for food
-
-const mimiFactory: CatFactory = new MimiFactory();
-const mimi: Cat = mimiFactory.createCat();
-console.log('Mimi likes to', mimi.action()); 
-// Mimi bites my toes at 3 am
+const romad = catFactory('Romad');
+const mimi = catFactory('Mimi');
+console.log(`Romad likes to ${romad.action()}`);
+console.log(`Mimi likes to ${mimi.action()}`);
+// const notACat = catFactory('notACat');
